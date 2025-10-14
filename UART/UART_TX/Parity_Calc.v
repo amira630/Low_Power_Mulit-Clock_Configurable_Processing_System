2026764fb96294ce52d8bc6641da4abf24734a51
amira630 <a.amiraelkomy@outlook.com>
2025-10-14 17:58:24 +0300

    Updated Tx Parity_Calc

--- UART/UART_TX/Parity_Calc.v
+++ UART/UART_TX/Parity_Calc.v
@@ -1,19 +1,28 @@
 module Parity_Calc(
+    input  wire       clk,
+    input  wire       rst_n,
     input  wire [7:0] P_DATA,
-    input  wire       Data_Valid,
+    input  wire       Capture,
     input  wire       Par_Type, // 0: Even, 1: Odd
     output reg        parity_out
 );
+
+    reg [7:0] data_reg;
+
     // Parity Calculator
-    always @(*) begin
-        if (Data_Valid) begin
-            if (Par_Type) begin
-                parity_out = ~^P_DATA; // Odd Parity
-            end else begin
-                parity_out = ^P_DATA; // Even Parity
-            end
-        end else begin
-            parity_out = 1'b0; // Default value when data is not valid
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            parity_out <= 1'b0;
+            data_reg <= 8'b0;
+        end
+        else if (Capture) begin
+            data_reg <= P_DATA;
+        end
+        else begin
+            if (Par_Type) // Odd Parity
+                parity_out <= ~^data_reg;
+            else          // Even Parity
+                parity_out <= ^data_reg;
         end
     end
 endmodule
